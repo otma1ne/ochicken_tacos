e7053455826cfe1ee8dfc87075e653607897a948
Mimooun <zouhrimimoun21@gmail.com>
2024-03-02 18:28:04 +0100

    sales products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
   return (
     <div>
       <Home_page />
-      <Wrapper/>
-      <Sales/>
+      <Wrapper />
+      <Sales />
     </div>
   );
 }
---
+++ src/shared/components/Sales/Sales.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Sales.module.scss"
+function sales() {
+  return (
+    <div>
+      <div className={styles.title}>Hot sales</div>
+    </div>
+  )
+}
+
+export default sales;
