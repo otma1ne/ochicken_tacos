baec07bca75974b260ae0453dbb00860c1d16e5b
Mimooun <zouhrimimoun21@gmail.com>
2024-03-02 17:18:25 +0100

    wrp

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Menu from "./pages/Menu/Menu";
-import Wrapper from "./shared/components/Wrapper/wrapper";
 
 const router = createBrowserRouter([
   {
--- src/shared/components/Wrapper/wrapper.jsx
+++ src/shared/components/Wrapper/wrapper.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import styles from "./Wrapper.module.scss";
-import ico from "../../../assets/icons/icon_2.png";
+import ico1 from "../../../assets/icons/icon_1.png";
+import ico2 from "../../../assets/icons/icon_2.png";
+import ico3 from "../../../assets/icons/icon_3.png";
+
 
 function wrapper() {
   return (
+  
     <div className={styles.wrp}>
-      wraaper hhhh
-      <img src={ico} alt="" />
+      <div className={styles.img_container}>
+        <div className={styles.wrp_container}>
+          <div className={styles.item}>
+            <img src={ico1} alt="" />
+            <div className={styles.title}>Quality Foods</div>
+            <div className={styles.desc}>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</div>
+          </div>
+          <div className={styles.item}>
+            <img src={ico3} alt="" />
+            <div className={styles.title}>Fastest Delivery</div>
+            <div className={styles.desc}>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</div>
+          </div>
+          <div className={styles.item}>
+            <img src={ico2} alt="" />
+            <div className={styles.title}>Original Recipes </div>
+            <div className={styles.desc}>Sit amet, consectetur adipiscing elit quisque eget maximus velit, non eleifend libero curabitur dapibus mauris sed leo cursus aliquetcras suscipit.</div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
