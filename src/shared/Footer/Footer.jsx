da36518856567f04644764a87ed1e67ed9bcfc09
Mimooun <zouhrimimoun21@gmail.com>
2024-03-04 00:02:53 +0100

    styles

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Footer.module.scss";
-import logo from "../../../assets/images/logo-ochiken.png"
+import logo from "../../assets/images/logo-ochiken.png"
 
 function Footer() {
   return (
