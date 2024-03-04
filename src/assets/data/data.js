6ec6d2885d857ffa0601e3b9d1165d3cc45f2e3f
Mimooun <zouhrimimoun21@gmail.com>
2024-03-04 02:09:52 +0100

    fichier data

---
+++ src/assets/data/data.js
@@ -0,0 +1,42 @@
+// import pizza1Image from "../images/Pizza/4fromages.png";
+import pizza from "../images/Pizza/3jambons.png"
+const data = [
+    
+    {
+        "id": 1,
+        "name": "pizza1",
+        "imageUrl": pizza,
+        "price": 35,
+        "category": "pizza"
+    },
+    {
+        "id": 2,
+        "name": "pizza2",
+        "imageUrl": "dddddd",
+        "price": 45,
+        "category": "pizza"
+    },
+    {
+        "id": 3,
+        "name": "pizza3",
+        "imageUrl": "....",
+        "price": 25,
+        "category": "pizza"
+    },
+    {
+        "id": 4,
+        "name": "burger1",
+        "imageUrl": "....",
+        "price": 15,
+        "category": "burger"
+    },
+    {
+        "id": 5,
+        "name": "burger2",
+        "imageUrl": "....",
+        "price": 15,
+        "category": "burger"
+    }
+]
+
+export default data;
