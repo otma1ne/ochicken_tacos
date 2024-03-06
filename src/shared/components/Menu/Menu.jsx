f5c642dc278dbfea01205794f0363489fe55dc55
Mimooun <zouhrimimoun21@gmail.com>
2024-03-07 00:05:14 +0100

    responsive

--- src/shared/components/Menu/Menu.jsx
+++ src/shared/components/Menu/Menu.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import styles from './Menu.module.scss';
 import data from "../../../assets/data/data.js"
 
-
 export default function MyTabs() {
     const [activeCategory, setActiveCategory] = useState('pizza')
     return (
@@ -13,16 +12,16 @@ export default function MyTabs() {
                         <div className={`${styles.filter_item} ${activeCategory === 'pizza' ? styles.active : ''}`} onClick={() => { setActiveCategory('pizza') }}>Pizza</div>
                         <div className={`${styles.filter_item} ${activeCategory === 'burger' ? styles.active : ''}`} onClick={() => { setActiveCategory('burger') }}>Burger</div>
                         <div className={`${styles.filter_item} ${activeCategory === 'sandwiche' ? styles.active : ''}`} onClick={() => { setActiveCategory('sandwiche') }}>Assiets</div>
-                        <div className={`${styles.filter_item} ${activeCategory === 'sandwiche' ? styles.active : ''}`} onClick={() => { setActiveCategory('sandwiche') }}>Sandwiche</div>
-                        <div className={`${styles.filter_item} ${activeCategory === 'sandwiche' ? styles.active : ''}`} onClick={() => { setActiveCategory('sandwiche') }}>Bucket</div>
-                        <div className={`${styles.filter_item} ${activeCategory === 'sandwiche' ? styles.active : ''}`} onClick={() => { setActiveCategory('sandwiche') }}>boisson</div>
+                        <div className={`${styles.filter_item} ${activeCategory === 'assiets' ? styles.active : ''}`} onClick={() => { setActiveCategory('assiets') }}>Sandwiche</div>
+                        <div className={`${styles.filter_item} ${activeCategory === 'Bucket' ? styles.active : ''}`} onClick={() => { setActiveCategory('Bucket') }}>Bucket</div>
+                        <div className={`${styles.filter_item} ${activeCategory === 'Supliments' ? styles.active : ''}`} onClick={() => { setActiveCategory('Supliments') }}>boisson</div>
                     </div>
                 </div>
 
                 <div className={styles.cards__container}>
                     {data.filter((item) => item.category === activeCategory).map((item) => {
                         return <div className={styles.card}>
-                            <div className={styles.img}><img src={item.imageUrl} alt="" /></div>
+                           <img src={item.imageUrl} alt="image produit" /> 
                             <div className={styles.title}>{item.name}</div>
                             <div className={styles.ingre}>{item.ingredient}</div>
                             <div className={styles.prix}>{item.price}</div>
